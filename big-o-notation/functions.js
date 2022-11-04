3b68cb3359773886624d7bb2e0d0f4832053522e
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-11-03 21:28:03 -0300

    add functions

--- big-o-notation/functions.js
+++ big-o-notation/functions.js
@@ -1,4 +1,22 @@
 // O(1) -> this function is constant because the performance of the function is always the same
 function increment(n) {
-    return ++num
+  return ++num;
+}
+
+// O(n) -> time of executation grow up according the number of n, because is any number
+function countUp(n) {
+  for (let i = 0; i < n; i++) {
+    console.log(i);
+  }
+}
+
+// O(n²) -> is quadratic because it has two nested loops
+function printAllPairs(n) {
+  for (var i = 0; i < n; i++) {
+    // O(n) -> grows according to the n
+    for (var j = 0; j < n; j++) {
+      // O(n) -> grows according to the n
+      console.log({ i, j });
+    }
+  }
 }
