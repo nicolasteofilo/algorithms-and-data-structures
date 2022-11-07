ca71bebc002146f8c049544bc7f125d525f8a8fb
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-11-07 16:23:59 -0300

    add object performance

---
+++ big-o-notation/array-and-objecets-performance/object.js
@@ -0,0 +1,17 @@
+const instructor = {
+    firstName: "Kelly",
+    isInstructor: true,
+    favoriteNumbers: [1,2,3,4]
+}
+
+/*
+    Insertion - O(1)
+    Removal - O(1)
+    Search - O(n)
+    Access - O(1)
+*/
+
+
+console.log(instructor)
+console.log(Object.keys(instructor))
+
