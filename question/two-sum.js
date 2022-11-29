fd198f29594b3db9a75d422c993479afbc395810
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-11-29 14:55:14 -0300

    refactor: thorow error in two sum

--- question/two-sum.js
+++ question/two-sum.js
@@ -5,6 +5,9 @@
     Output: One number, which corresponds to the sum of the two input numbers
 */
 
-function twoSum(oneParam, twoParam) {
-    return oneParam + twoParam
+function twoSum(...params) {
+  if (params.length !== 2) {
+    return `    ${"\x1b[31m"}Error:${"\x1b[0m"} The entry must contain two numbers`;
+  }
+  return params[0] + params[1];
 }
