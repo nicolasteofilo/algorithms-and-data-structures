e393f77f1a15d34042d0ef90a64dac2f08dd1409
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-11-29 14:43:52 -0300

    add question: two sum

---
+++ question/two-sum.js
@@ -0,0 +1,10 @@
+/*
+    Question: Write a function which takes two numbers and returns their sum.
+
+    Input: Two numbers
+    Output: One number, which corresponds to the sum of the two input numbers
+*/
+
+function twoSum(oneParam, twoParam) {
+    return oneParam + twoParam
+}
