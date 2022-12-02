e3c21e88184a7e51e94370ec6ffa0f2e291f23bc
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-12-02 12:41:42 -0300

    add question: countUniqueValues

--- patters/multiple-pointers.js
+++ patters/multiple-pointers.js
@@ -29,6 +29,5 @@ function sumZero(nums) {
 }
 
 console.log(sumZero([-3, -2, -1, 0, 1, 2, 3])); // [-3,3]
-
 console.log(sumZero([-2, 0, 1, 3])); // undefined
 console.log(sumZero([1, 2, 3])); // undefined
