e1fc52a3bee3060ee879415ceb73b1275f1ad112
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-12-22 11:01:24 -0300

    optimize bubble sort

--- sorting/bubble-sort.js
+++ sorting/bubble-sort.js
@@ -9,17 +9,22 @@ function swap(arr, idx1, idx2) {
  * @return {number[]}
 */
 function bubbleSort(arr) {
+  let noSwaps;
   for (let i = arr.length; i > 0; i--) {
+    noSwaps = true;
     for (let j = 0; j < i - 1; j++) {
       if (arr[j + 1]) {
-        console.log(arr, arr[j], arr[j+1]);
-        if (arr[j] > arr[j + 1]) swap(arr, j, j+1) 
+        if (arr[j] > arr[j + 1]) {
+          swap(arr, j, j+1);
+          noSwaps = false;
+        }
       }
     }
+  if(noSwaps) break;
   }
-
   return arr;
 };
 
 console.log('[ 6, 4, 15, 10 ] ->', bubbleSort([6,4,15,10])); // [ 4, 6, 10, 15 ]
+console.log('[ 8, 1, 2 ] ->', bubbleSort([8,1,2])); // [ 1, 2, 8 ]
 
