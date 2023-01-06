a953933745ee9a444c4a724d72988ab139929c15
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2023-01-06 13:26:33 -0300

    implemet quick sort

--- sorting/quick-sort.js
+++ sorting/quick-sort.js
@@ -11,7 +11,7 @@ function swap(arr, idx1, idx2) {
  * @param {number} end
  */
 function pivot(arr, start = 0, end = arr.length + 1) {
-  let pivot = arr[0];
+  let pivot = arr[start];
   let swapIndex = start;
 
   for (let i = start + 1; i < arr.length; i++) {
@@ -25,5 +25,21 @@ function pivot(arr, start = 0, end = arr.length + 1) {
   return swapIndex;
 }
 
+/**
+ * 
+ * @param {number[]} arr 
+ */
+function quickSort(arr, left = 0, right = arr.length - 1) {
+  if(left < right) {
+    let pivotIndex = pivot(arr, left, right);
+    quickSort(arr, left, pivotIndex - 1) // to the left side
+    quickSort(arr, pivotIndex + 1, right) // to the right side
+  }
+
+  return arr;
+}
+
 let arr = [ 5, 2, 1, 8, 4, 7, 6, 3 ]
-console.log(pivot(arr)); // 4
+let arr2 = [ 5 ]
+console.log(quickSort(arr));
+console.log(quickSort(arr2));
