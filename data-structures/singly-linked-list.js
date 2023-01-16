bcb2ba12c784c22d94c86719369e0fe5371fa295
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2023-01-16 14:57:34 -0300

    feat: add push() to SinglyLinkedList

--- data-structures/singly-linked-list.js
+++ data-structures/singly-linked-list.js
@@ -4,3 +4,24 @@ class Node {
     this.next = null
   }
 }
+
+class SinglyLinkedList {
+  constructor() {
+    this.length = 0;
+    this.head = null;
+    this.tail = null;
+  }
+
+  push(val) {
+    const node = new Node(val);
+    if (this.length <= 0) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.tail.next = node;
+      this.tail = node;
+    }
+    this.length++;
+    return node;
+  }
+}
