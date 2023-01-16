ebce6e3ef821142209b3fa1879ee6dd7a7d574e9
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2023-01-16 15:38:26 -0300

    feat: add pop() to SinglyLinkedList

--- data-structures/singly-linked-list.js
+++ data-structures/singly-linked-list.js
@@ -24,4 +24,24 @@ class SinglyLinkedList {
     this.length++;
     return node;
   }
+
+  pop() {
+    if (!this.head) return undefined;
+    let current = this.head;
+    let newTail = current;
+
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return current;
+  }
 }
