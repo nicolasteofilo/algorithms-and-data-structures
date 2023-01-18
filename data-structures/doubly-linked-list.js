c0ffa11427a7282552b2f657360c80d79f113df5
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2023-01-18 11:06:31 -0300

    feat: add push() to DoublyLinkedList

--- data-structures/doubly-linked-list.js
+++ data-structures/doubly-linked-list.js
@@ -12,4 +12,19 @@ class DoublyLinkedList {
     this.head = null;
     this.tail = null;
   }
+
+  push(val) {
+    const node = new Node(val);
+    if (!this.head) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.tail.next = node;
+      node.prev = this.tail;
+      this.tail = node;
+    }
+
+    this.length++;
+    return node;
+  }
 }
