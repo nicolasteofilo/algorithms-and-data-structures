f3e085412fecde26298b002bcfb3530a60c1384f
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2023-02-07 21:48:05 -0300

    feat: implemeting a queue with array

---
+++ data-structures/queues.js
@@ -0,0 +1,12 @@
+const queue = [];
+queue.push("Harry");
+queue.push("Ron");
+queue.push("Hermione");
+
+console.log(queue)
+queue.shift();
+console.log(queue);
+
+/*
+FIFO (first-in first-out), primeiro elemento a ser inserido, é o primeiro a ser retirado.
+*/
