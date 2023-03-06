d992461b7cf9a6f81168889a8db6e6c8633a042b
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2023-03-06 06:02:43 -0300

    feat: insertion into binary search tree

--- trees/binary-search-trees.js
+++ trees/binary-search-trees.js
@@ -10,10 +10,42 @@ class BinarySearchTree {
   constructor() {
     this.root = null;
   }
+
+  insert(value) {
+    const newNode = new Node(value);
+
+    if(!this.root) {
+      this.root = newNode;
+      return this;
+    } else {
+      let currentNode = this.root;
+      while(true) {
+        if (value < currentNode.value) {
+          if(currentNode.left === null) {
+            currentNode.left = newNode;
+            return this;
+          } else {
+            currentNode = currentNode.left;
+          }
+        } else if (value > currentNode.value) {
+          if (currentNode.right === null) {
+            currentNode.right = newNode;
+            return this;
+          } else {
+            currentNode = currentNode.right;
+          }
+        } else if(value === currentNode.value) {
+          return undefined;
+        }
+      }
+    }
+  }
 }
 
 const tree = new BinarySearchTree();
-tree.root = new Node(10);
-tree.root.right = new Node(12);
-tree.root.left = new Node(8);
-console.log(tree);
+tree.insert(10);
+tree.insert(3);
+tree.insert(4);
+tree.insert(2);
+
+console.log(JSON.stringify(tree, null, 2));
