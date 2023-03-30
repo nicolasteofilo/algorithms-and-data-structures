00d48b867cd1068789ffd27f64ec6a4ea822627c
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2023-03-29 21:44:57 -0300

    feat: implemeting values() in hash table

--- hash-tables/hash-table.js
+++ hash-tables/hash-table.js
@@ -1,5 +1,5 @@
 class HashTable {
-  constructor(size=53){
+  constructor(size = 53) {
     this.keyMap = new Array(size);
   }
 
@@ -8,39 +8,48 @@ class HashTable {
     let WEIRD_PRIME = 31;
     for (let i = 0; i < Math.min(key.length, 100); i++) {
       let char = key[i];
-      let value = char.charCodeAt(0) - 96
+      let value = char.charCodeAt(0) - 96;
       total = (total * WEIRD_PRIME + value) % this.keyMap.length;
     }
     return total;
   }
 
-  set(key,value){
+  set(key, value) {
     let index = this._hash(key);
-    if(!this.keyMap[index]){
+    if (!this.keyMap[index]) {
       this.keyMap[index] = [];
     }
     this.keyMap[index].push([key, value]);
   }
 
-  get(key){
+  get(key) {
     let index = this._hash(key);
-    if(this.keyMap[index]){
-      for(let i = 0; i < this.keyMap[index].length; i++){
-        if(this.keyMap[index][i][0] === key) {
-          return this.keyMap[index][i][1]
+    if (this.keyMap[index]) {
+      for (let i = 0; i < this.keyMap[index].length; i++) {
+        if (this.keyMap[index][i][0] === key) {
+          return this.keyMap[index][i][1];
         }
       }
     }
     return undefined;
   }
 
-
   keys() {
     const values = [];
 
     this.keyMap.forEach((element) => {
       element.forEach((subElement) => values.push(subElement[0]));
-    })
+    });
+
+    return values;
+  }
+
+  values() {
+    const values = [];
+
+    this.keyMap.forEach((element) => {
+      element.forEach((subElement) => values.push(subElement[1]));
+    });
 
     return values;
   }
@@ -53,3 +62,4 @@ ht.set('yellow', '#ffff00');
 ht.set('red', '#ff0000');
 // console.log(ht.keyMap)
 console.log(ht.keys())
+console.log(ht.values());
