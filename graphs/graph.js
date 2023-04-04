8752476e4c856b25572661fe668e5ab62392fc2d
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2023-04-03 21:28:31 -0300

    feat: removeEdge() method in graph

--- graphs/graph.js
+++ graphs/graph.js
@@ -13,6 +13,13 @@ class Graph {
       this.adjacencyList[vertex2].push(vertex1);
     }
   }
+
+  removeEdge(vertex1, vertex2) {
+    if (this.adjacencyList[vertex1] && this.adjacencyList[vertex2]) {
+      this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter((vertex) => vertex !== vertex2);
+      this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter((vertex) => vertex !== vertex1);
+    }
+  }
 }
 
 const g = new Graph();
@@ -20,4 +27,8 @@ g.addVertex("Harry");
 g.addVertex("Hermione");
 g.addVertex("Rony");
 g.addEdge("Harry", "Rony");
+g.addEdge("Harry", "Hermione");
+console.log(g.adjacencyList);
+console.log('-------');
+g.removeEdge("Harry", "Rony");
 console.log(g.adjacencyList);
