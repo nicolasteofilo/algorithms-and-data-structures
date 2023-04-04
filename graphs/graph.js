c9aaa3b922513da87b14eeb9c6bda28f4605db2d
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2023-04-03 21:20:33 -0300

    feat: addVertex() method in graph

--- graphs/graph.js
+++ graphs/graph.js
@@ -2,4 +2,15 @@ class Graph {
   constructor() {
     this.adjacencyList = {};
   }
+
+  addVertex(vertexName) {
+    if (!this.adjacencyList[vertexName]) this.adjacencyList[vertexName] = [];
+  }
 }
+
+const g = new Graph();
+g.addVertex("Harry");
+g.addVertex("Hermione");
+g.addVertex("Rony");
+g.addEdge("Harry", "Rony");
+console.log(g.adjacencyList);
