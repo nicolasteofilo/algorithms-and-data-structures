51bad9fecb5ea75b685bebbebf0ed24b8c23e0b7
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2023-04-26 21:01:47 -0300

    docs: add fib time complexity

--- dynamic-programming/fib.js
+++ dynamic-programming/fib.js
@@ -6,6 +6,10 @@ function fib(number) {
   return fib(number - 1) + fib(number - 2);
 }
 
+/*
+Big O of old fibonacci sequence solution using recursive algorithm
+O(2^n) => complexity time grows very fast
+*/
 const before = Date.now();
 console.log(fib(40));
 const time = Date.now() - before;
